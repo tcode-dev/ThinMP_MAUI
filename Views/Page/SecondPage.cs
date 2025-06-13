c1383d4968d6033b00330c7835f6e1c357c14025
tcode <tk@tcode.dev>
2025-06-13 17:33:40 +0900

    refactor SecondPage to use CollectionView for item display

--- Views/Page/SecondPage.cs
+++ Views/Page/SecondPage.cs
@@ -1,23 +1,42 @@
+using System.Collections.ObjectModel;
 using CommunityToolkit.Maui.Markup;
 
 class SecondPage : ContentPage
 {
     public SecondPage()
     {
-        Content = new VerticalStackLayout
-        {
-            Children =
-            {
-                new Label()
-                    .Text("Second Page")
-                    .FontSize(24)
-                    .CenterHorizontal(),
+        // Content = new VerticalStackLayout
+        // {
+        //     Children =
+        //     {
+        //         new Label()
+        //             .Text("Second Page")
+        //             .FontSize(24)
+        //             .CenterHorizontal(),
 
-                new Button()
-                    .Text("Back")
-                    .CenterHorizontal()
-                    .Invoke(b => b.Clicked += (s, e) => Navigation.PopAsync())
-            }
+        //         new Button()
+        //             .Text("Back")
+        //             .CenterHorizontal()
+        //             .Invoke(b => b.Clicked += (s, e) => Navigation.PopAsync()),
+
+        //     }
+        // };
+        var items = new ObservableCollection<string>
+        {
+            "aa", "bb", "cc"
         };
+
+        Content = new CollectionView
+        {
+            ItemsSource = items,
+            ItemTemplate = new DataTemplate(() =>
+                new Label()
+                    .Bind(Label.TextProperty, ".")
+                    .FontSize(20)
+                    .Padding(10)
+            )
+        }
+        .Margin(20);
+
     }
 }
