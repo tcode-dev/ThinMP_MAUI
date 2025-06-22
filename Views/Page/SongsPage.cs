86585d828a3f1e030a209fb14174debaf9770cb2
tcode <tk@tcode.dev>
2025-06-23 08:30:09 +0900

    feat: enhance SongsPage by adding tap gesture recognition for song items

--- Views/Page/SongsPage.cs
+++ Views/Page/SongsPage.cs
@@ -1,5 +1,6 @@
 using System.Collections.ObjectModel;
 using CommunityToolkit.Maui.Markup;
+using ThinMPm.Contracts.Models;
 using ThinMPm.ViewModels;
 
 namespace ThinMPm.Views.Page;
@@ -14,7 +15,8 @@ class SongsPage : ContentPage
         Content = new CollectionView
         {
             ItemTemplate = new DataTemplate(() =>
-                new Grid
+            {
+                var grid = new Grid
                 {
                     ColumnDefinitions =
                     {
@@ -31,17 +33,32 @@ class SongsPage : ContentPage
                         new Image()
                             .Bind(Image.SourceProperty, "ImageId")
                             .Row(0).RowSpan(2).Column(0),
-
                         new Label()
                             .Bind(Label.TextProperty, "Name")
                             .Row(0).Column(1),
-
                         new Label()
                             .Bind(Label.TextProperty, "ArtistName")
                             .Row(1).Column(1)
                     }
-                }
-            )
+                };
+
+                var tapGesture = new TapGestureRecognizer();
+
+                tapGesture.Tapped += (s, e) =>
+                {
+                    if (s is BindableObject bindable && BindingContext is SongViewModel vm)
+                    {
+                        if (bindable.BindingContext is ISongModel item)
+                        {
+                            int index = vm.Songs.IndexOf(item);
+                            Console.WriteLine($"Tapped index: {index}, Name: {item.Name}");
+                        }
+                    }
+                };
+                grid.GestureRecognizers.Add(tapGesture);
+
+                return grid;
+            })
         }.Bind(ItemsView.ItemsSourceProperty, nameof(vm.Songs));
     }
 
