c3870329bac6f1661e0ca32354b84cb5075e7db1
tcode <tk@tcode.dev>
2025-07-10 17:34:31 +0900

    feat: implement AlbumArtService for retrieving album artwork

---
+++ Platforms/iOS/Services/AlbumArtService.cs
@@ -0,0 +1,11 @@
+using ThinMPm.Contracts.Services;
+
+namespace ThinMPm.Platforms.iOS.Services;
+
+public class AlbumArtService : IAlbumArtService
+{
+    public async Task<string?> GetArtwork(string id)
+    {
+        return null;
+    }
+}
