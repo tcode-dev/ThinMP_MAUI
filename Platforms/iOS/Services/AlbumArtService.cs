081191cfb068db0396d98914b86ed875abf7e71d
tcode <tk@tcode.dev>
2025-07-11 08:33:49 +0900

    feat: implement GetArtwork method in AlbumArtService for retrieving album artwork

--- Platforms/iOS/Services/AlbumArtService.cs
+++ Platforms/iOS/Services/AlbumArtService.cs
@@ -1,4 +1,6 @@
+using CoreGraphics;
 using ThinMPm.Contracts.Services;
+using ThinMPm.Platforms.iOS.Repositories.Contracts;
 
 namespace ThinMPm.Platforms.iOS.Services;
 
@@ -6,6 +8,21 @@ public class AlbumArtService : IAlbumArtService
 {
     public async Task<string?> GetArtwork(string id)
     {
-        return null;
+        var repository = Application.Current?.Handler?.MauiContext?.Services.GetRequiredService<ISongRepository>();
+        var song = repository.FindBySongId(id);
+        var image = song?.Artwork?.ImageWithSize(new CGSize(100, 100));
+        if (image == null)
+        {
+            return null;
+        }
+
+        using var data = image.AsPNG();
+
+        if (data == null)
+        {
+            return null;
+        }
+
+        return Convert.ToBase64String(data.ToArray());
     }
 }
