a194250ccce0119015bea1297e826a1e491afe72
tcode <tk@tcode.dev>
2025-07-16 00:01:09 +0900

    feat: replace grid layout with SongListItem component for song display

--- Views/Page/SongsPage.cs
+++ Views/Page/SongsPage.cs
@@ -2,7 +2,7 @@ using CommunityToolkit.Maui.Markup;
 using ThinMPm.Contracts.Models;
 using ThinMPm.Contracts.Services;
 using ThinMPm.ViewModels;
-using ThinMPm.Views.Img;
+using ThinMPm.Views.Row;
 
 namespace ThinMPm.Views.Page;
 
@@ -19,39 +19,8 @@ class SongsPage : ContentPage
         {
             ItemTemplate = new DataTemplate(() =>
             {
-                var grid = new Grid
-                {
-                    ColumnDefinitions =
-                    {
-                        new ColumnDefinition { Width = 40 },
-                        new ColumnDefinition { Width = GridLength.Star }
-                    },
-                    RowDefinitions =
-                    {
-                        new RowDefinition { Height = GridLength.Auto },
-                        new RowDefinition { Height = GridLength.Auto }
-                    },
-                    Children =
-                    {
-                        new ArtworkImg()
-                        {
-                            WidthRequest = 44,
-                            HeightRequest = 44
-                        }
-                            .Bind(ArtworkImg.IdProperty, "ImageId")
-                            .Row(0).RowSpan(2).Column(0),
-
-                        new Label()
-                            .Bind(Label.TextProperty, "Name")
-                            .Row(0).Column(1),
-                        new Label()
-                            .Bind(Label.TextProperty, "ArtistName")
-                            .Row(1).Column(1)
-                    }
-                };
-
+                var itemView = new SongListItem();
                 var tapGesture = new TapGestureRecognizer();
-
                 tapGesture.Tapped += (s, e) =>
                 {
                     if (s is BindableObject bindable && BindingContext is SongViewModel vm)
@@ -59,14 +28,13 @@ class SongsPage : ContentPage
                         if (bindable.BindingContext is ISongModel item)
                         {
                             int index = vm.Songs.IndexOf(item);
-
                             _playerService.StartAllSongs(index);
                         }
                     }
                 };
-                grid.GestureRecognizers.Add(tapGesture);
+                itemView.GestureRecognizers.Add(tapGesture);
 
-                return grid;
+                return itemView;
             })
         }.Bind(ItemsView.ItemsSourceProperty, nameof(vm.Songs));
     }
--- Views/Row/SongListItem.cs
+++ Views/Row/SongListItem.cs
@@ -1,13 +1,38 @@
 using CommunityToolkit.Maui.Markup;
 using ThinMPm.Views.Img;
-using Microsoft.Maui.Controls;
 
-namespace ThinMPm.Views.Components;
+namespace ThinMPm.Views.Row;
 
 public class SongListItem : Grid
 {
     public SongListItem()
     {
+        ColumnDefinitions.Add(new ColumnDefinition { Width = 40 });
+        ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Star });
 
+        RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
+        RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
+
+        Children.Add(
+            new ArtworkImg
+            {
+                WidthRequest = 44,
+                HeightRequest = 44
+            }
+            .Bind(ArtworkImg.IdProperty, "ImageId")
+            .Row(0).RowSpan(2).Column(0)
+        );
+
+        Children.Add(
+            new Label()
+                .Bind(Label.TextProperty, "Name")
+                .Row(0).Column(1)
+        );
+
+        Children.Add(
+            new Label()
+                .Bind(Label.TextProperty, "ArtistName")
+                .Row(1).Column(1)
+        );
     }
 }
