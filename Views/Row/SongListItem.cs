5de7d8c73cda8f3d0d10b5ab864caac89629aaab
tcode <tk@tcode.dev>
2025-07-15 18:18:22 +0900

    feat: add SongListItem component as a Grid for displaying song details

---
+++ Views/Row/SongListItem.cs
@@ -0,0 +1,13 @@
+using CommunityToolkit.Maui.Markup;
+using ThinMPm.Views.Img;
+using Microsoft.Maui.Controls;
+
+namespace ThinMPm.Views.Components;
+
+public class SongListItem : Grid
+{
+    public SongListItem()
+    {
+
+    }
+}
