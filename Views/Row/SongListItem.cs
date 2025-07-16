1f6c12913305932ceb1a8b62c0b389a32de1d8ef
tcode <tk@tcode.dev>
2025-07-17 08:30:44 +0900

    refactor: adjust padding and dimensions in SongListItem for consistency

--- Views/Row/SongListItem.cs
+++ Views/Row/SongListItem.cs
@@ -11,6 +11,9 @@ public class SongListItem : Grid
         tapGesture.Tapped += tappedHandler;
         GestureRecognizers.Add(tapGesture);
 
+        HeightRequest = 50;
+        Padding = new Thickness(20, 0, 0, 0);
+
         ColumnDefinitions.Add(new ColumnDefinition { Width = 40 });
         ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Star });
 
@@ -20,8 +23,8 @@ public class SongListItem : Grid
         Children.Add(
             new ArtworkImg
             {
-                WidthRequest = 44,
-                HeightRequest = 44
+                WidthRequest = 40,
+                HeightRequest = 40
             }
             .Bind(ArtworkImg.IdProperty, "ImageId")
             .Row(0).RowSpan(2).Column(0)
@@ -31,12 +34,14 @@ public class SongListItem : Grid
             new Label()
                 .Bind(Label.TextProperty, "Name")
                 .Row(0).Column(1)
+                .Margin(new Thickness(10, 0, 0, 0))
         );
 
         Children.Add(
             new Label()
                 .Bind(Label.TextProperty, "ArtistName")
                 .Row(1).Column(1)
+                .Margin(new Thickness(10, 0, 0, 0))
         );
     }
 }
