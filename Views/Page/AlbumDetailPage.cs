5050ff1050eb0e4912c96f460bb07c2be308cc6e
tcode <tk@tcode.dev>
2025-07-25 08:12:20 +0900

    feat: implement AlbumDetailPage for displaying album details and song interaction

---
+++ Views/Page/AlbumDetailPage.cs
@@ -0,0 +1,53 @@
+using CommunityToolkit.Maui.Markup;
+using ThinMPm.Contracts.Models;
+using ThinMPm.Contracts.Services;
+using ThinMPm.ViewModels;
+using ThinMPm.Views.Row;
+using ThinMPm.Views.Header;
+
+namespace ThinMPm.Views.Page;
+
+class AlbumDetailPage : ContentPage
+{
+    private readonly IPlayerService _playerService;
+    public AlbumDetailPage(SongViewModel vm, IPlayerService playerService)
+    {
+        NavigationPage.SetHasNavigationBar(this, false);
+
+        BindingContext = vm;
+        _playerService = playerService;
+        Content = new VerticalStackLayout
+        {
+            Children = {
+                new SongsHeader(),
+                new CollectionView
+                {
+                    ItemTemplate = new DataTemplate(() => new SongListItem(OnSongTapped))
+                }
+                .Bind(ItemsView.ItemsSourceProperty, nameof(vm.Songs))
+            }
+        };
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+
+        if (BindingContext is SongViewModel vm)
+        {
+            vm.Load();
+        }
+    }
+
+    private void OnSongTapped(object? sender, EventArgs e)
+    {
+        if (sender is BindableObject bindable && BindingContext is SongViewModel vm)
+        {
+            if (bindable.BindingContext is ISongModel item)
+            {
+                int index = vm.Songs.IndexOf(item);
+                _playerService.StartAllSongs(index);
+            }
+        }
+    }
+}
