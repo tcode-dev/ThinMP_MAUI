48fed65cb47ead21e43541cb4de8092da04cfc41
tcode <tk@tcode.dev>
2025-07-30 07:49:09 +0900

    refactor: adjust method formatting and improve FindById implementation in AlbumRepository

--- Platforms/iOS/Repositories/AlbumRepository.cs
+++ Platforms/iOS/Repositories/AlbumRepository.cs
@@ -7,8 +7,8 @@ namespace ThinMPm.Platforms.iOS.Repositories;
 
 public class AlbumRepository : IAlbumRepository
 {
-public IList<IAlbumModel> FindAll()
-{
+  public IList<IAlbumModel> FindAll()
+  {
     var predicate = new MPMediaPropertyPredicate();
     var query = MPMediaQuery.AlbumsQuery;
 
@@ -17,18 +17,23 @@ public IList<IAlbumModel> FindAll()
     var collections = query.Collections;
     if (collections == null)
     {
-        return new List<IAlbumModel>();
+      return new List<IAlbumModel>();
     }
 
     return collections.Select(media => new AlbumModel(media)).Cast<IAlbumModel>().ToList();
-}
-
-  public IList<IAlbumModel> FindByArtistId(string artistId)
-  {
-    throw new NotImplementedException();
   }
 
   public IAlbumModel? FindById(string id)
+  {
+    var predicate = new MPMediaPropertyPredicate();
+    var query = MPMediaQuery.AlbumsQuery;
+
+    query.AddFilterPredicate(predicate);
+
+    return query.Collections!.Select(media => new AlbumModel(media)).FirstOrDefault();
+  }
+
+  public IList<IAlbumModel> FindByArtistId(string artistId)
   {
     throw new NotImplementedException();
   }
