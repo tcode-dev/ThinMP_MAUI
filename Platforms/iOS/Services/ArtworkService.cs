3217b32bc1808c0c391a7b5c5b0a38167ade9204
tcode <tk@tcode.dev>
2025-08-11 10:47:13 +0900

    refactor: update ArtworkService to use Id class in GetArtwork method

--- Platforms/iOS/Services/ArtworkService.cs
+++ Platforms/iOS/Services/ArtworkService.cs
@@ -1,6 +1,7 @@
 using CoreGraphics;
 using ThinMPm.Contracts.Services;
 using ThinMPm.Platforms.iOS.Repositories.Contracts;
+using ThinMPm.Platforms.iOS.ValueObjects;
 
 namespace ThinMPm.Platforms.iOS.Services;
 
@@ -9,7 +10,7 @@ public class ArtworkService : IArtworkService
     public async Task<byte[]?> GetArtwork(string id)
     {
         var repository = Application.Current?.Handler?.MauiContext?.Services.GetRequiredService<ISongRepository>();
-        var song = repository?.FindBySongId(id);
+        var song = repository?.FindById(new Id(id));
         var image = song?.Artwork?.ImageWithSize(new CGSize(100, 100));
         var data = image?.AsPNG();
 
