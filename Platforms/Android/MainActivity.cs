e6ea7ed43f609669d2ba798bb3a3aba51397a421
tcode <tk@tcode.dev>
2025-09-06 01:40:11 +0900

    fix: update MainActivity to set system window decorations and enable fullscreen layout

--- Platforms/Android/MainActivity.cs
+++ Platforms/Android/MainActivity.cs
@@ -2,6 +2,9 @@
 using Android.Content.PM;
 using Android.OS;
 using Android.Views;
+using AndroidX.Core.Content;
+using AndroidX.Core.View;
+using Microsoft.Maui.Controls;
 
 namespace ThinMPm;
 
@@ -14,5 +17,20 @@ public class MainActivity : MauiAppCompatActivity
 
         // Window.SetStatusBarColor(Android.Graphics.Color.Transparent);
         // Window.DecorView.SystemUiVisibility = (StatusBarVisibility)(SystemUiFlags.LayoutStable | SystemUiFlags.LayoutFullscreen);
+
+        // システムウィンドウのデコレーションを無効化
+        WindowCompat.SetDecorFitsSystemWindows(Window, false);
+
+        // レイアウトを画面全体に拡張
+        Window.SetFlags(WindowManagerFlags.LayoutNoLimits, WindowManagerFlags.LayoutNoLimits);
+
+        // ステータスバーを透過
+        Window.SetStatusBarColor(new Android.Graphics.Color(ContextCompat.GetColor(this, Android.Resource.Color.Transparent)));
+
+        // // display cutout領域にも表示
+        // var attributes = Window.Attributes;
+        // attributes.LayoutInDisplayCutoutMode = (int)WindowManagerLayoutParams.LayoutInDisplayCutoutModeShortEdges;
+        // Window.Attributes = attributes;
+
     }
 }
