36803f1be725397bbc771aff6c2c3ade947284cb
tcode <tk@tcode.dev>
2025-09-14 18:13:52 +0900

    refactor: enhance AlbumDetailPage layout and scrolling functionality

--- Views/Page/AlbumDetailPage.cs
+++ Views/Page/AlbumDetailPage.cs
@@ -5,6 +5,8 @@ using ThinMPm.ViewModels;
 using ThinMPm.Views.Row;
 using ThinMPm.Views.Header;
 using ThinMPm.Views.Img;
+using ThinMPm.Contracts.Utils;
+using Microsoft.Maui.Layouts;
 
 namespace ThinMPm.Views.Page;
 
@@ -12,27 +14,52 @@ class AlbumDetailPage : ContentPage
 {
     public string AlbumId { get; set; }
     private readonly IPlayerService _playerService;
-    public AlbumDetailPage(AlbumDetailViewModel vm, IPlayerService playerService)
+
+    private readonly IPlatformUtil _platformUtil;
+
+    public AlbumDetailPage(AlbumDetailViewModel vm, IPlayerService playerService, IPlatformUtil platformUtil)
     {
         NavigationPage.SetHasNavigationBar(this, false);
 
         BindingContext = vm;
         _playerService = playerService;
+        _platformUtil = platformUtil;
 
-        Content = new VerticalStackLayout
+        var layout = new AbsoluteLayout();
+        var header = new ArtistDetailHeader().Bind(ArtistDetailHeader.TitleProperty, "Album.Name");
+
+        AbsoluteLayout.SetLayoutFlags(header, AbsoluteLayoutFlags.WidthProportional);
+        AbsoluteLayout.SetLayoutBounds(header, new Rect(0, 0, 1, 100));
+
+        var scrollView = new ScrollView
         {
-            Children = {
-                new AlbumDetailHeader()
-                    .Bind(AlbumDetailHeader.TitleProperty, "Album.Name"),
+            Content = new VerticalStackLayout
+            {
+                Children = {
                 new ArtworkImg()
                     .Bind(ArtworkImg.IdProperty, "Album.ImageId"),
+                    new SongsHeader(),
                 new CollectionView
                 {
                     ItemTemplate = new DataTemplate(() => new SongListItem(OnSongTapped))
                 }
                     .Bind(ItemsView.ItemsSourceProperty, "Songs")
+                }
             }
         };
+        scrollView.Scrolled += OnScrolled;
+
+        AbsoluteLayout.SetLayoutFlags(scrollView, AbsoluteLayoutFlags.All);
+        AbsoluteLayout.SetLayoutBounds(scrollView, new Rect(0, 0, 1, 1));
+
+        // AbsoluteLayoutの重なり順はChildrenの追加順で決まる
+        layout.Children.Add(scrollView);
+        layout.Children.Add(header);
+
+        layout.IgnoreSafeArea = true;
+        layout.Padding = new Thickness(0, _platformUtil.GetLayoutNegativeMargin(), 0, 0);
+
+        Content = layout;
     }
 
     protected override void OnAppearing()
@@ -55,5 +82,12 @@ class AlbumDetailPage : ContentPage
                 _playerService.StartAllSongs(index);
             }
         }
+    }
+
+    private void OnScrolled(object? sender, ScrolledEventArgs e)
+    {
+        double x = e.ScrollX;
+        double y = e.ScrollY;
+        Console.WriteLine($"Scrolled to position: ({x}, {y})");
     }
 }
