a40f2c5f886033b535b830fc2e4ef10ece1e972e
tcode <tk@tcode.dev>
2025-09-16 08:24:43 +0900

    refactor: improve formatting and organization in ArtworkImg class

--- Views/Img/ArtworkImg.cs
+++ Views/Img/ArtworkImg.cs
@@ -1,3 +1,5 @@
+
+using Microsoft.Maui.Controls.Shapes;
 using ThinMPm.Contracts.Services;
 
 namespace ThinMPm.Views.Img;
@@ -5,16 +7,40 @@ namespace ThinMPm.Views.Img;
 public class ArtworkImg : Image
 {
   public static readonly BindableProperty IdProperty =
-      BindableProperty.Create(
-          nameof(Id),
-          typeof(string),
-          typeof(ArtworkImg),
-          propertyChanged: OnIdChanged);
+    BindableProperty.Create(
+      nameof(Id),
+      typeof(string),
+      typeof(ArtworkImg),
+      propertyChanged: OnIdChanged);
 
   public string Id
   {
     get => (string)GetValue(IdProperty);
     set => SetValue(IdProperty, value);
+  }
+
+  public double CornerRadius { get; set; }
+
+  public ArtworkImg(double cornerRadius = 5)
+  {
+    CornerRadius = cornerRadius;
+    SizeChanged += OnSizeChanged;
+  }
+
+  private void OnSizeChanged(object? sender, EventArgs e)
+  {
+    if (Width > 0 && Height > 0 && CornerRadius > 0)
+    {
+      Clip = new RoundRectangleGeometry
+      {
+        CornerRadius = CornerRadius,
+        Rect = new Rect(0, 0, Width, Height)
+      };
+    }
+    else
+    {
+      Clip = null;
+    }
   }
 
   private static async void OnIdChanged(BindableObject bindable, object oldValue, object newValue)
@@ -38,8 +64,8 @@ public class ArtworkImg : Image
     {
       var artwork = await artworkService.GetArtwork(id);
       control.Source = artwork != null
-          ? ImageSource.FromStream(() => new MemoryStream(artwork))
-          : null;
+        ? ImageSource.FromStream(() => new MemoryStream(artwork))
+        : null;
     }
     catch
     {
