fc660a218c0133e987ed0fa867538184b8f742c7
tcode <tk@tcode.dev>
2025-10-10 18:04:38 +0900

    refactor: simplify ArtistModel constructor for improved readability

--- Platforms/Android/Models/ArtistModel.cs
+++ Platforms/Android/Models/ArtistModel.cs
@@ -2,16 +2,10 @@ using ThinMPm.Platforms.Android.Models.Contracts;
 
 namespace ThinMPm.Platforms.Android.Models;
 
-public class ArtistModel : IArtistModel
+public class ArtistModel(
+    string id,
+    string name) : IArtistModel
 {
-    public string Id { get; }
-    public string Name { get; }
-
-    public ArtistModel(
-        string id,
-        string name)
-    {
-        Id = id;
-        Name = name;
-    }
+  public string Id { get; } = id;
+  public string Name { get; } = name;
 }
