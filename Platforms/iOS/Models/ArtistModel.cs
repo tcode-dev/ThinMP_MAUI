714a36462687837ec24aede3d715a85f4c3c5568
tcode <tk@tcode.dev>
2025-10-15 20:50:00 +0900

    refactor: simplify ArtistModel constructor for improved readability

--- Platforms/iOS/Models/ArtistModel.cs
+++ Platforms/iOS/Models/ArtistModel.cs
@@ -3,18 +3,13 @@ using ThinMPm.Platforms.iOS.Models.Contracts;
 
 namespace ThinMPm.Platforms.iOS.Models;
 
-public class ArtistModel : IArtistModel
+public class ArtistModel(MPMediaItemCollection media) : IArtistModel
 {
-    public MPMediaItemCollection Media { get; }
+  public MPMediaItemCollection Media { get; } = media;
 
-    public string Id => (Media.RepresentativeItem?.ArtistPersistentID ?? 0).ToString();
+  public string Id => (Media.RepresentativeItem?.ArtistPersistentID ?? 0).ToString();
 
     public string Name => !string.IsNullOrEmpty(Media.RepresentativeItem?.Artist)
         ? Media.RepresentativeItem.Artist
         : "undefined";
-
-    public ArtistModel(MPMediaItemCollection media)
-    {
-        Media = media;
-    }
 }
