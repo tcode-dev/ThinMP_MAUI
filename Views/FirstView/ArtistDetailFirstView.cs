ae5985d1d191a11a29632c47c7b1481c22b7ecce
tcode <tk@tcode.dev>
2025-10-21 09:07:10 +0900

    feat: add ArtistDetailFirstView to manage artist details layout

---
+++ Views/FirstView/ArtistDetailFirstView.cs
@@ -0,0 +1,11 @@
+using CommunityToolkit.Maui.Markup;
+
+namespace ThinMPm.Views.FirstView;
+
+class ArtistDetailFirstView : AbsoluteLayout
+{
+    public ArtistDetailFirstView()
+    {
+        NavigationPage.SetHasNavigationBar(this, false);
+    }
+}
