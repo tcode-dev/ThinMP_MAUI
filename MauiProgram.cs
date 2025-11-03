2b47572f10cf4d176db066e072d34399270079b4
tcode <tk@tcode.dev>
2025-11-03 09:26:20 +0900

    fix: standardize indentation in MauiProgram.cs for improved readability

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -20,47 +20,47 @@ namespace ThinMPm;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
 
 #if ANDROID || IOS
-		builder.Services.AddSingleton<ISongRepository, SongRepository>();
-		builder.Services.AddSingleton<IAlbumRepository, AlbumRepository>();
-		builder.Services.AddSingleton<IArtistRepository, ArtistRepository>();
+        builder.Services.AddSingleton<ISongRepository, SongRepository>();
+        builder.Services.AddSingleton<IAlbumRepository, AlbumRepository>();
+        builder.Services.AddSingleton<IArtistRepository, ArtistRepository>();
 
-		builder.Services.AddSingleton<ISongService, SongService>();
-		builder.Services.AddSingleton<IAlbumService, AlbumService>();
-		builder.Services.AddSingleton<IArtistService, ArtistService>();
-		builder.Services.AddSingleton<IArtworkService, ArtworkService>();
-		builder.Services.AddSingleton<IPlayerService, PlayerService>();
+        builder.Services.AddSingleton<ISongService, SongService>();
+        builder.Services.AddSingleton<IAlbumService, AlbumService>();
+        builder.Services.AddSingleton<IArtistService, ArtistService>();
+        builder.Services.AddSingleton<IArtworkService, ArtworkService>();
+        builder.Services.AddSingleton<IPlayerService, PlayerService>();
 
-		builder.Services.AddSingleton<IPlatformUtil, PlatformUtili>();
+        builder.Services.AddSingleton<IPlatformUtil, PlatformUtili>();
 #endif
 
-		builder.Services.AddTransient<ArtistViewModel>();
-		builder.Services.AddTransient<AlbumViewModel>();
-		builder.Services.AddTransient<SongViewModel>();
-		builder.Services.AddTransient<ArtistDetailViewModel>();
-		builder.Services.AddTransient<AlbumDetailViewModel>();
+        builder.Services.AddTransient<ArtistViewModel>();
+        builder.Services.AddTransient<AlbumViewModel>();
+        builder.Services.AddTransient<SongViewModel>();
+        builder.Services.AddTransient<ArtistDetailViewModel>();
+        builder.Services.AddTransient<AlbumDetailViewModel>();
 
-		builder.Services.AddTransient<ArtistsPage>();
-		builder.Services.AddTransient<AlbumsPage>();
-		builder.Services.AddTransient<SongsPage>();
-		builder.Services.AddTransient<ArtistDetailPage>();
-		builder.Services.AddTransient<AlbumDetailPage>();
+        builder.Services.AddTransient<ArtistsPage>();
+        builder.Services.AddTransient<AlbumsPage>();
+        builder.Services.AddTransient<SongsPage>();
+        builder.Services.AddTransient<ArtistDetailPage>();
+        builder.Services.AddTransient<AlbumDetailPage>();
 
 #if DEBUG
-		builder.Logging.AddDebug();
+        builder.Logging.AddDebug();
 #endif
 
-		return builder.Build();
-	}
+        return builder.Build();
+    }
 }
