79cd9d69724443f0457bb918da456259def3963d
tcode <tk@tcode.dev>
2025-11-06 18:49:52 +0900

    feat: implement AlbumDetailFirstView layout with image and text bindings

--- Views/FirstView/AlbumDetailFirstView.cs
+++ Views/FirstView/AlbumDetailFirstView.cs
@@ -1,9 +1,41 @@
+using CommunityToolkit.Maui.Markup;
+using Microsoft.Maui.Layouts;
+using ThinMPm.Views.Img;
+
 namespace ThinMPm.Views.FirstView;
 
 class AlbumDetailFirstView : AbsoluteLayout
 {
     public AlbumDetailFirstView()
     {
+        var image = new ArtworkImage()
+            .Bind(ArtworkImage.ImageIdProperty, "ImageId");
+        var primaryText = new Label
+        {
+            HeightRequest = 50,
+            FontAttributes = FontAttributes.Bold,
+            HorizontalTextAlignment = TextAlignment.Center,
+            VerticalTextAlignment = TextAlignment.Center
+        }
+        .Bind(Label.TextProperty, "Album.Name");
+        var secondaryText = new Label()
+            .Bind(Label.TextProperty, "Album.ArtistName")
+            .Font(bold: true)
+            .Center();
 
+        AbsoluteLayout.SetLayoutFlags(primaryText, AbsoluteLayoutFlags.PositionProportional);
+        AbsoluteLayout.SetLayoutBounds(primaryText, new Rect(0.5, 0.8, AbsoluteLayout.AutoSize, AbsoluteLayout.AutoSize));
+
+        AbsoluteLayout.SetLayoutFlags(secondaryText, AbsoluteLayoutFlags.PositionProportional);
+        AbsoluteLayout.SetLayoutBounds(secondaryText, new Rect(0.5, 0.9, AbsoluteLayout.AutoSize, AbsoluteLayout.AutoSize));
+
+        Children.Add(image);
+        Children.Add(primaryText);
+        Children.Add(secondaryText);
+
+        this.SizeChanged += (s, e) =>
+        {
+            HeightRequest = this.Width;
+        };
     }
 }
