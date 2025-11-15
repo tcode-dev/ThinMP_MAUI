f2a6409a03b02219039bdfbda7ca9334434e82e3
tcode <tk@tcode.dev>
2025-11-16 00:02:25 +0900

    feat: enhance MainHeader layout with dynamic status bar height and grid structure

--- Views/Header/MainHeader.cs
+++ Views/Header/MainHeader.cs
@@ -1,9 +1,43 @@
+using CommunityToolkit.Maui.Markup;
+using ThinMPm.Contracts.Utils;
+
 namespace ThinMPm.Views.Header;
 
 public class MainHeader : ContentView
 {
     public MainHeader()
     {
-        Content = new Header { Title = "Library" };
+        var platformUtil = Application.Current?.Handler?.MauiContext?.Services
+            .GetRequiredService<IPlatformUtil>();
+
+        BackgroundColor = Colors.White;
+
+        var statusBarHeight = platformUtil?.GetStatusBarHeight() ?? 0;
+
+        HeightRequest = 60 + statusBarHeight;
+        Padding = new Thickness(0, statusBarHeight, 0, 0);
+
+        Content = new Grid
+        {
+            ColumnDefinitions =
+            {
+                new ColumnDefinition(20),
+                new ColumnDefinition(GridLength.Star),
+                new ColumnDefinition(50)
+            },
+            Children =
+            {
+                new Label()
+                {
+                    HeightRequest = 50,
+                    FontAttributes = FontAttributes.Bold,
+                    TextColor = Colors.Black,
+                    FontSize = 32,
+                    VerticalTextAlignment = TextAlignment.Center,
+                    Text = "Library"
+                }
+                .Column(1)
+            }
+        };
     }
 }
