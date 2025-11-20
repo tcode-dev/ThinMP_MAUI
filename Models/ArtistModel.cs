fd060fabc0a21f72f6abcafaa91e4dce709582f2
tcode <tk@tcode.dev>
2025-11-20 09:20:41 +0900

    refactor: simplify ArtistModel constructor and property initialization

--- Models/ArtistModel.cs
+++ Models/ArtistModel.cs
@@ -2,14 +2,8 @@ using ThinMPm.Contracts.Models;
 
 namespace ThinMPm.Models;
 
-public class ArtistModel : IArtistModel
+public class ArtistModel(string id, string name) : IArtistModel
 {
-  public ArtistModel(string id, string name)
-  {
-    Id = id;
-    Name = name;
-  }
-
-  public string Id { get; set; } = string.Empty;
-  public string Name { get; set; } = string.Empty;
+  public string Id { get; set; } = id;
+  public string Name { get; set; } = name;
 }
