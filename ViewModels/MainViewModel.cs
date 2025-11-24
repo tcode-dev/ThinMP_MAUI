86c8a074ae9a995d3d865ec1d845a957c77ebca6
tcode <tk@tcode.dev>
2025-11-24 09:22:58 +0900

    refactor: replace List with ObservableCollection for MenuItems and streamline Load method

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -1,19 +1,37 @@
+using System.Collections.ObjectModel;
 using ThinMPm.Contracts.Models;
+using ThinMPm.Contracts.Services;
 using ThinMPm.Models;
 
 namespace ThinMPm.ViewModels;
 
 public class MainViewModel
 {
-    public List<IMenuModel> MenuItems { get; }
+    readonly IAlbumService _albumService;
+    public ObservableCollection<IMenuModel> MenuItems { get; } = [];
+    public ObservableCollection<IAlbumModel> Albums { get; } = [];
 
     public MainViewModel()
     {
-        MenuItems = new List<IMenuModel>
+        var services = Application.Current?.Handler?.MauiContext?.Services;
+        _albumService = services?.GetRequiredService<IAlbumService>() 
+            ?? throw new InvalidOperationException("IAlbumService is not registered");
+    }
+
+    public void Load()
+    {
+        MenuItems.Clear();
+        MenuItems.Add(new MenuModel("Artists", nameof(Views.Page.ArtistsPage)));
+        MenuItems.Add(new MenuModel("Albums", nameof(Views.Page.AlbumsPage)));
+        MenuItems.Add(new MenuModel("Songs", nameof(Views.Page.SongsPage)));
+
+        var albums = _albumService.FindByRecent(20);
+
+        Albums.Clear();
+
+        foreach (var album in albums)
         {
-            new MenuModel("Artists", nameof(Views.Page.ArtistsPage)),
-            new MenuModel("Albums", nameof(Views.Page.AlbumsPage)),
-            new MenuModel("Songs", nameof(Views.Page.SongsPage)),
-        };
+            Albums.Add(album);
+        }
     }
 }
