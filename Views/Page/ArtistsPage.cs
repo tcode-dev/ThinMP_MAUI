4a31713ee15b40d13b96717576809773fdf82541
tcode <tk@tcode.dev>
2025-12-03 08:44:14 +0900

    refactor: enhance ArtistsPage layout by integrating AbsoluteLayout and replacing ArtistsHeader with EmptyHeader

--- Views/Page/ArtistsPage.cs
+++ Views/Page/ArtistsPage.cs
@@ -1,18 +1,32 @@
 using CommunityToolkit.Maui.Markup;
-using ThinMPm.ViewModels;
-using ThinMPm.Views.Row;
-using ThinMPm.Views.Header;
+using Microsoft.Maui.Layouts;
 using ThinMPm.Contracts.Models;
+using ThinMPm.Contracts.Utils;
+using ThinMPm.ViewModels;
+using ThinMPm.Views.Header;
+using ThinMPm.Views.Row;
 
 namespace ThinMPm.Views.Page;
 
 class ArtistsPage : ContentPage
 {
-    public ArtistsPage(ArtistViewModel vm)
+    private readonly IPlatformUtil _platformUtil;
+    private readonly ArtistsHeader header;
+    public ArtistsPage(ArtistViewModel vm, IPlatformUtil platformUtil)
     {
         Shell.SetNavBarIsVisible(this, false);
 
         BindingContext = vm;
+        _platformUtil = platformUtil;
+
+        var layout = new AbsoluteLayout
+        {
+            SafeAreaEdges = SafeAreaEdges.None,
+        };
+        var statusBarHeight = _platformUtil?.GetStatusBarHeight() ?? 0;
+        header = new ArtistsHeader();
+        AbsoluteLayout.SetLayoutFlags(header, AbsoluteLayoutFlags.WidthProportional);
+        AbsoluteLayout.SetLayoutBounds(header, new Rect(0, 0, 1, statusBarHeight + 50));
 
         var scrollView = new ScrollView
         {
@@ -20,7 +34,7 @@ class ArtistsPage : ContentPage
             Content = new VerticalStackLayout
             {
                 Children = {
-                    new ArtistsHeader(),
+                    new EmptyHeader(),
                     new CollectionView
                     {
                         ItemTemplate = new DataTemplate(() => new ArtistListItem(OnTapped))
@@ -31,7 +45,13 @@ class ArtistsPage : ContentPage
         };
         scrollView.Scrolled += OnScrolled;
 
-        Content = scrollView;
+        AbsoluteLayout.SetLayoutFlags(scrollView, AbsoluteLayoutFlags.All);
+        AbsoluteLayout.SetLayoutBounds(scrollView, new Rect(0, 0, 1, 1));
+
+        layout.Children.Add(scrollView);
+        layout.Children.Add(header);
+
+        Content = layout;
     }
 
     protected override void OnAppearing()
