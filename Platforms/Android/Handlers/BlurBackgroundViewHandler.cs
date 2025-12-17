3a07844cf67d64ad20337041da41be4e9ad88daf
tcode <tk@tcode.dev>
2025-12-17 21:50:06 +0900

    refactor: rename alias for Android.Views.View to improve code clarity in BlurBackgroundViewHandler

--- Platforms/Android/Handlers/BlurBackgroundViewHandler.cs
+++ Platforms/Android/Handlers/BlurBackgroundViewHandler.cs
@@ -2,11 +2,11 @@ using Microsoft.Maui.Handlers;
 using Microsoft.Maui.Platform;
 using ThinMPm.Constants;
 using ThinMPm.Views.Background;
-using AView = Android.Views.View;
+using AndroidView = Android.Views.View;
 
 namespace ThinMPm.Platforms.Android.Handlers;
 
-public class BlurBackgroundViewHandler : ViewHandler<BlurBackgroundView, AView>
+public class BlurBackgroundViewHandler : ViewHandler<BlurBackgroundView, AndroidView>
 {
     public static IPropertyMapper<BlurBackgroundView, BlurBackgroundViewHandler> PropertyMapper =
         new PropertyMapper<BlurBackgroundView, BlurBackgroundViewHandler>(ViewHandler.ViewMapper);
@@ -15,9 +15,9 @@ public class BlurBackgroundViewHandler : ViewHandler<BlurBackgroundView, AView>
     {
     }
 
-    protected override AView CreatePlatformView()
+    protected override AndroidView CreatePlatformView()
     {
-        var view = new AView(Context);
+        var view = new AndroidView(Context);
         var backgroundColor = ColorConstants.GetSecondaryBackgroundColor();
         view.SetBackgroundColor(backgroundColor.ToPlatform());
         return view;
