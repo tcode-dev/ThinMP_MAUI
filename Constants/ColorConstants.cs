186aacc656059cb1c10b7ffebf11549239e6a4af
tcode <tk@tcode.dev>
2025-12-20 20:19:06 +0900

    fix: correct IconColor definition placement in ColorConstants for consistency

--- Constants/ColorConstants.cs
+++ Constants/ColorConstants.cs
@@ -3,12 +3,11 @@ namespace ThinMPm.Constants;
 public static class ColorConstants
 {
     public static bool IsDarkMode => Application.Current?.RequestedTheme == AppTheme.Dark;
-
     public static Color PrimaryBackgroundColor => IsDarkMode ? Colors.Black : Colors.White;
     public static Color SecondaryBackgroundColor => IsDarkMode ? Color.FromArgb("#1C1C1E") : Colors.WhiteSmoke;
     public static Color PrimaryTextColor => IsDarkMode ? Colors.White : Colors.Black;
-    public static Color IconColor => IsDarkMode ? Colors.White : Colors.Black;
     public static Color SecondaryTextColor => IsDarkMode ? Colors.WhiteSmoke : Color.FromArgb("#1C1C1E");
     public static Color GradientColor => IsDarkMode ? Colors.Black : Colors.White;
+    public static Color IconColor => IsDarkMode ? Colors.White : Colors.Black;
     public static Color LineColor => IsDarkMode ? Colors.DarkGray : Colors.LightGray;
 }
