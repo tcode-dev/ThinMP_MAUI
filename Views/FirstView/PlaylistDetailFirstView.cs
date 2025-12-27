de872dd6d1b06309545b07bb51d19dbc6bf199ea
tcode <tk@tcode.dev>
2025-12-27 14:20:34 +0900

    feat: add secondary title to PlaylistDetailFirstView for enhanced UI display

--- Views/FirstView/PlaylistDetailFirstView.cs
+++ Views/FirstView/PlaylistDetailFirstView.cs
@@ -1,6 +1,7 @@
 using CommunityToolkit.Maui.Markup;
 using Microsoft.Maui.Layouts;
 using ThinMPm.Constants;
+using ThinMPm.Resources.Strings;
 using ThinMPm.Views.Img;
 using ThinMPm.Views.Text;
 
@@ -9,6 +10,7 @@ namespace ThinMPm.Views.FirstView;
 class PlaylistDetailFirstView : AbsoluteLayout
 {
     private readonly PrimaryTitle primaryTitle;
+    private readonly SecondaryTitle secondaryTitle;
 
     public PlaylistDetailFirstView()
     {
@@ -19,12 +21,15 @@ class PlaylistDetailFirstView : AbsoluteLayout
         AbsoluteLayout.SetLayoutBounds(gradientOverlay, new Rect(0, 0, 1, 1));
 
         primaryTitle = new PrimaryTitle().Bind(Label.TextProperty, "Playlist.Name");
+        secondaryTitle = new SecondaryTitle { Text = AppResources.Playlist };
 
         AbsoluteLayout.SetLayoutFlags(primaryTitle, AbsoluteLayoutFlags.XProportional);
+        AbsoluteLayout.SetLayoutFlags(secondaryTitle, AbsoluteLayoutFlags.XProportional);
 
         Children.Add(image);
         Children.Add(gradientOverlay);
         Children.Add(primaryTitle);
+        Children.Add(secondaryTitle);
 
         this.SizeChanged += (s, e) =>
         {
@@ -35,6 +40,9 @@ class PlaylistDetailFirstView : AbsoluteLayout
 
             var primaryTitleY = this.Height * LayoutConstants.HeaderVisibilityThreshold;
             AbsoluteLayout.SetLayoutBounds(primaryTitle, new Rect(0.5, primaryTitleY, AbsoluteLayout.AutoSize, AbsoluteLayout.AutoSize));
+
+            var secondaryTitleY = primaryTitleY + 40;
+            AbsoluteLayout.SetLayoutBounds(secondaryTitle, new Rect(0.5, secondaryTitleY, AbsoluteLayout.AutoSize, AbsoluteLayout.AutoSize));
         };
     }
 }
