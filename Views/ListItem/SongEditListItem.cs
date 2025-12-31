5639b497a85bf7c2a0665206a2411856a0e7e46e
tcode <tk@tcode.dev>
2025-12-31 12:29:48 +0900

    feat: refactor FavoriteSongsEditPage to use SongEditListItem for improved item management; remove obsolete swipe functionality

---
+++ Views/ListItem/SongEditListItem.cs
@@ -0,0 +1,123 @@
+using CommunityToolkit.Maui.Markup;
+using ThinMPm.Constants;
+using ThinMPm.Contracts.Models;
+using ThinMPm.Resources.Strings;
+using ThinMPm.Views.Behaviors;
+using ThinMPm.Views.Img;
+using ThinMPm.Views.Text;
+using ThinMPm.Views.Utils;
+
+namespace ThinMPm.Views.ListItem;
+
+public class SongEditListItem : SwipeView
+{
+    private readonly Action<ISongModel> _deleteCallback;
+
+    public SongEditListItem(Action<ISongModel> deleteCallback)
+    {
+        _deleteCallback = deleteCallback;
+
+        var deleteSwipeItem = new SwipeItem
+        {
+            Text = AppResources.Delete,
+            BackgroundColor = Colors.Red,
+        };
+        deleteSwipeItem.Invoked += OnDeleteSwipeItemInvoked;
+
+        RightItems = new SwipeItems { deleteSwipeItem };
+        Content = CreateSongItemContent();
+    }
+
+    private Grid CreateSongItemContent()
+    {
+        var grid = new Grid
+        {
+            Padding = new Thickness(LayoutConstants.SpacingLarge, 0, 0, 0),
+            BackgroundColor = ColorConstants.PrimaryBackgroundColor,
+            ColumnDefinitions =
+            {
+                new ColumnDefinition { Width = LayoutConstants.ImageSize },
+                new ColumnDefinition { Width = GridLength.Star },
+                new ColumnDefinition { Width = LayoutConstants.ButtonMedium },
+            },
+            RowDefinitions =
+            {
+                new RowDefinition { Height = LayoutConstants.HeightSmall },
+                new RowDefinition { Height = LayoutConstants.HeightSmall },
+                new RowDefinition { Height = 1 },
+            }
+        };
+
+        grid.Children.Add(
+            new ArtworkImage()
+                .Width(LayoutConstants.ImageSize)
+                .Height(LayoutConstants.ImageSize)
+                .Bind(ArtworkImage.ImageIdProperty, nameof(ISongModel.ImageId))
+                .Row(0)
+                .RowSpan(2)
+                .Column(0)
+        );
+
+        grid.Children.Add(
+            new PrimaryText()
+                .Bind(Label.TextProperty, nameof(ISongModel.Name))
+                .Margin(new Thickness(LayoutConstants.SpacingMedium, LayoutConstants.SpacingSmall, 0, 0))
+                .CenterVertical()
+                .Row(0)
+                .Column(1)
+        );
+
+        grid.Children.Add(
+            new SecondaryText()
+                .Bind(Label.TextProperty, nameof(ISongModel.ArtistName))
+                .Margin(new Thickness(LayoutConstants.SpacingMedium, 0, 0, LayoutConstants.SpacingSmall))
+                .CenterVertical()
+                .Row(1)
+                .Column(1)
+        );
+
+        var dragHandle = CreateDragHandle();
+        dragHandle.Row(0).RowSpan(2).Column(2);
+        grid.Children.Add(dragHandle);
+
+        grid.Children.Add(
+            new Separator()
+                .Row(2)
+                .ColumnSpan(3)
+        );
+
+        return grid;
+    }
+
+    private static Grid CreateDragHandle()
+    {
+        var dragIcon = new Image
+        {
+            Source = "drag",
+            WidthRequest = LayoutConstants.ButtonExtraSmall,
+            HeightRequest = LayoutConstants.ButtonExtraSmall,
+            HorizontalOptions = LayoutOptions.Center,
+            VerticalOptions = LayoutOptions.Center,
+        };
+        dragIcon.Behaviors.Add(new IconColorBehavior { TintColor = ColorConstants.IconColor });
+
+        var container = new Grid
+        {
+            WidthRequest = LayoutConstants.ButtonMedium,
+            HeightRequest = LayoutConstants.ButtonMedium,
+            HorizontalOptions = LayoutOptions.Center,
+            VerticalOptions = LayoutOptions.Center,
+            Children = { dragIcon }
+        };
+
+        return container;
+    }
+
+    private void OnDeleteSwipeItemInvoked(object? sender, EventArgs e)
+    {
+        if (BindingContext is ISongModel song)
+        {
+            _deleteCallback(song);
+        }
+    }
+}
--- Views/Page/FavoriteSongsEditPage.cs
+++ Views/Page/FavoriteSongsEditPage.cs
@@ -1,15 +1,11 @@
 using CommunityToolkit.Maui.Markup;
 using Microsoft.Maui.Layouts;
-using ThinMPm.Constants;
 using ThinMPm.Contracts.Models;
 using ThinMPm.Contracts.Utils;
-using ThinMPm.Resources.Strings;
 using ThinMPm.ViewModels;
-using ThinMPm.Views.Behaviors;
 using ThinMPm.Views.Header;
-using ThinMPm.Views.Img;
+using ThinMPm.Views.ListItem;
 using ThinMPm.Views.Player;
-using ThinMPm.Views.Text;
 using ThinMPm.Views.Utils;
 
 namespace ThinMPm.Views.Page;
@@ -38,7 +34,7 @@ class FavoriteSongsEditPage : ContentPage
 
         var collectionView = new CollectionView
         {
-            ItemTemplate = new DataTemplate(() => CreateSwipeableItem()),
+            ItemTemplate = new DataTemplate(() => new SongEditListItem(OnDeleteRequested)),
             Header = new HeaderSpacer(),
             Footer = new FooterSpacer(),
             CanReorderItems = true,
@@ -61,109 +57,6 @@ class FavoriteSongsEditPage : ContentPage
         Content = layout;
     }
 
-    private SwipeView CreateSwipeableItem()
-    {
-        var deleteSwipeItem = new SwipeItem
-        {
-            Text = AppResources.Delete,
-            BackgroundColor = Colors.Red,
-        };
-        deleteSwipeItem.Invoked += OnDeleteSwipeItemInvoked;
-
-        var swipeView = new SwipeView
-        {
-            RightItems = new SwipeItems { deleteSwipeItem },
-            Content = CreateSongItemContent()
-        };
-
-        return swipeView;
-    }
-
-    private Grid CreateSongItemContent()
-    {
-        var grid = new Grid
-        {
-            Padding = new Thickness(LayoutConstants.SpacingLarge, 0, 0, 0),
-            BackgroundColor = ColorConstants.PrimaryBackgroundColor,
-            ColumnDefinitions =
-            {
-                new ColumnDefinition { Width = LayoutConstants.ImageSize },
-                new ColumnDefinition { Width = GridLength.Star },
-                new ColumnDefinition { Width = LayoutConstants.ButtonMedium },
-            },
-            RowDefinitions =
-            {
-                new RowDefinition { Height = LayoutConstants.HeightSmall },
-                new RowDefinition { Height = LayoutConstants.HeightSmall },
-                new RowDefinition { Height = 1 },
-            }
-        };
-
-        grid.Children.Add(
-            new ArtworkImage()
-                .Width(LayoutConstants.ImageSize)
-                .Height(LayoutConstants.ImageSize)
-                .Bind(ArtworkImage.ImageIdProperty, nameof(ISongModel.ImageId))
-                .Row(0)
-                .RowSpan(2)
-                .Column(0)
-        );
-
-        grid.Children.Add(
-            new PrimaryText()
-                .Bind(Label.TextProperty, nameof(ISongModel.Name))
-                .Margin(new Thickness(LayoutConstants.SpacingMedium, LayoutConstants.SpacingSmall, 0, 0))
-                .CenterVertical()
-                .Row(0)
-                .Column(1)
-        );
-
-        grid.Children.Add(
-            new SecondaryText()
-                .Bind(Label.TextProperty, nameof(ISongModel.ArtistName))
-                .Margin(new Thickness(LayoutConstants.SpacingMedium, 0, 0, LayoutConstants.SpacingSmall))
-                .CenterVertical()
-                .Row(1)
-                .Column(1)
-        );
-
-        var dragHandle = CreateDragHandle();
-        dragHandle.Row(0).RowSpan(2).Column(2);
-        grid.Children.Add(dragHandle);
-
-        grid.Children.Add(
-            new Separator()
-                .Row(2)
-                .ColumnSpan(3)
-        );
-
-        return grid;
-    }
-
-    private Grid CreateDragHandle()
-    {
-        var dragIcon = new Image
-        {
-            Source = "drag",
-            WidthRequest = LayoutConstants.ButtonExtraSmall,
-            HeightRequest = LayoutConstants.ButtonExtraSmall,
-            HorizontalOptions = LayoutOptions.Center,
-            VerticalOptions = LayoutOptions.Center,
-        };
-        dragIcon.Behaviors.Add(new IconColorBehavior { TintColor = ColorConstants.IconColor });
-
-        var container = new Grid
-        {
-            WidthRequest = LayoutConstants.ButtonMedium,
-            HeightRequest = LayoutConstants.ButtonMedium,
-            HorizontalOptions = LayoutOptions.Center,
-            VerticalOptions = LayoutOptions.Center,
-            Children = { dragIcon }
-        };
-
-        return container;
-    }
-
     protected override async void OnAppearing()
     {
         base.OnAppearing();
@@ -174,14 +67,11 @@ class FavoriteSongsEditPage : ContentPage
         }
     }
 
-    private void OnDeleteSwipeItemInvoked(object? sender, EventArgs e)
+    private void OnDeleteRequested(ISongModel song)
     {
-        if (sender is SwipeItem swipeItem && swipeItem.BindingContext is ISongModel song)
+        if (BindingContext is FavoriteSongsEditViewModel vm)
         {
-            if (BindingContext is FavoriteSongsEditViewModel vm)
-            {
-                vm.RemoveSong(song);
-            }
+            vm.RemoveSong(song);
         }
     }
 
