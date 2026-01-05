bc9f98e7f64e7c6eab7e76d0928a322d9c8a3e2e
tcode <tk@tcode.dev>
2026-01-06 08:00:51 +0900

    refactor: implement activity lifecycle callbacks in MainApplication

--- Platforms/Android/MainApplication.cs
+++ Platforms/Android/MainApplication.cs
@@ -1,15 +1,42 @@
 ﻿using Android.App;
+using Android.OS;
 using Android.Runtime;
+using ThinMPm.Platforms.Android.Audio;
 
 namespace ThinMPm;
 
 [Application]
-public class MainApplication : MauiApplication
+public class MainApplication : MauiApplication, Android.App.Application.IActivityLifecycleCallbacks
 {
 	public MainApplication(IntPtr handle, JniHandleOwnership ownership)
 		: base(handle, ownership)
 	{
 	}
 
+	public override void OnCreate()
+	{
+		base.OnCreate();
+		RegisterActivityLifecycleCallbacks(this);
+	}
+
 	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+
+	public void OnActivityCreated(Activity activity, Bundle? savedInstanceState) { }
+
+	public void OnActivityStarted(Activity activity) { }
+
+	public void OnActivityResumed(Activity activity) { }
+
+	public void OnActivityPaused(Activity activity) { }
+
+	public void OnActivityStopped(Activity activity) { }
+
+	public void OnActivitySaveInstanceState(Activity activity, Bundle outState) { }
+
+	public void OnActivityDestroyed(Activity activity)
+	{
+		if (ApplicationContext == null) return;
+
+		MusicPlayer.Dispose(ApplicationContext);
+	}
 }
